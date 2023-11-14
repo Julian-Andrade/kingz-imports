b0670fb556b1a822d803eec1cb9aafeda6473f3a
Julian Andrade Silva <julian-andrade@hotmail.com>
2023-11-14 12:42:10 -0300

    "feat: add header menu using sheet component"

--- fsw-store/src/components/ui/header.tsx
+++ fsw-store/src/components/ui/header.tsx
@@ -1,13 +1,53 @@
-import { MenuIcon, ShoppingCartIcon } from "lucide-react";
+import {
+  MenuIcon,
+  ShoppingCartIcon,
+  MoveRight,
+  Percent,
+  Home,
+  ListOrdered,
+} from "lucide-react";
 import { Button } from "./button";
-import { Card, CardContent } from "./card";
+import { Card } from "./card";
+import { Sheet, SheetContent, SheetHeader, SheetTrigger } from "./sheet";
 
 export const Header = () => {
   return (
     <Card className="flex items-center justify-between p-[2rem]">
-      <Button size="icon" variant="outline">
-        <MenuIcon />
-      </Button>
+      <Sheet>
+        <SheetTrigger asChild>
+          <Button size="icon" variant="outline">
+            <MenuIcon />
+          </Button>
+        </SheetTrigger>
+
+        <SheetContent side="left">
+          <SheetHeader className="text-left text-lg font-semibold">
+            Menu
+          </SheetHeader>
+
+          <div className="mt-3 flex flex-col gap-3">
+            <Button variant="outline" className="w-full justify-start gap-2">
+              <MoveRight size={16} />
+              Entrar
+            </Button>
+
+            <Button variant="outline" className="w-full justify-start gap-2">
+              <Home size={16} />
+              Página Inicial
+            </Button>
+
+            <Button variant="outline" className="w-full justify-start gap-2">
+              <Percent size={16} />
+              Ofertas
+            </Button>
+
+            <Button variant="outline" className="w-full justify-start gap-2">
+              <ListOrdered size={16} />
+              Catálogo
+            </Button>
+          </div>
+        </SheetContent>
+      </Sheet>
 
       <h1 className="text-lg font-semibold">Andrade Store</h1>
 
