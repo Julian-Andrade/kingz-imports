fd162c594fe863c6dba481faec46712a07bea0cc
Julian Andrade Silva <julian-andrade@hotmail.com>
2023-11-20 11:01:35 -0300

    chore: add productTotalPrice utils function

---
+++ fsw-store/src/utils/product.ts
@@ -0,0 +1,24 @@
+import { Product } from "@prisma/client";
+
+interface ProductTotalPriceProps extends Product {
+  totalPrice: number;
+}
+
+const productTotalPrice = (product: Product): ProductTotalPriceProps => {
+  if (product.discountPercentage === 0) {
+    return {
+      ...product,
+      totalPrice: Number(product.basePrice),
+    };
+  }
+
+  const totalPrice =
+    Number(product.basePrice) * (product.discountPercentage / 100);
+
+  return {
+    ...product,
+    totalPrice,
+  };
+};
+
+export default productTotalPrice;
