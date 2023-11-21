68bfe87559e2261f85399354e679b4e176925917
Julian Andrade Silva <julian-andrade@hotmail.com>
2023-11-21 13:05:21 -0300

    feat:  add Footer component at page Home

---
+++ fsw-store/src/components/ui/footer.tsx
@@ -0,0 +1,11 @@
+const Footer = () => {
+  return (
+    <div className="mt-10 bg-zinc-200 px-5 py-7">
+      <p className="text-xs">
+        &copy; 2023 Copyright <span className="font-bold">Andrade Store</span>
+      </p>
+    </div>
+  );
+};
+
+export default Footer;
