9e48e5492f736892e923fb94757a14f363d17807
Julian Andrade Silva <julian-andrade@hotmail.com>
2023-11-21 11:26:20 -0300

    feat: add mouse and keyboards sections

--- fsw-store/src/app/(home)/page.tsx
+++ fsw-store/src/app/(home)/page.tsx
@@ -3,6 +3,7 @@ import Categories from "./components/categories/categories";
 import { prismaClient } from "@/lib/prisma";
 import ProductCarouselList from "./components/products/product-carousel-list";
 import SectionTitle from "@/components/ui/section-title";
+import PromoBanner from "@/components/ui/promo-banner";
 
 export default async function Home() {
   const deals = await prismaClient.product.findMany({
@@ -13,16 +14,27 @@ export default async function Home() {
     },
   });
 
+  const keyboards = await prismaClient.product.findMany({
+    where: {
+      category: {
+        slug: "keyboards",
+      },
+    },
+  });
+
+  const mouses = await prismaClient.product.findMany({
+    where: {
+      category: {
+        slug: "mouses",
+      },
+    },
+  });
+
   return (
     <div className="p-5">
       <div className="mt-7">
-        <Image
+        <PromoBanner
           src="/banner-home-01.png"
-          height={0}
-          width={0}
-          className="h-auto w-full"
-          sizes="100vw"
-          priority
           alt="Até 55% de desconto apenas este mês"
         />
       </div>
@@ -37,15 +49,27 @@ export default async function Home() {
       </div>
 
       <div className="mt-7">
-        <Image
+        <PromoBanner
           src="/banner-home-02.png"
-          height={0}
-          width={0}
-          className="h-auto w-full"
-          sizes="100vw"
-          priority
           alt="Até 55% de desconto em mouses"
         />
+      </div>
+
+      <div className="mt-7">
+        <SectionTitle title="Teclados" />
+        <ProductCarouselList products={keyboards} />
+      </div>
+
+      <div className="mt-7">
+        <PromoBanner
+          src="/banner-home-03.png"
+          alt="Até 20% de desconto em fones"
+        />
+      </div>
+
+      <div className="mt-7">
+        <SectionTitle title="Mouses" />
+        <ProductCarouselList products={mouses} />
       </div>
     </div>
   );
