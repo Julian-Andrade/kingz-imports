a1ea57b835aa88aa570bca688fb465e095b710bc
Julian Andrade <julian-andrade@hotmail.com>
2024-01-12 22:33:23 -0300

    feat: add variants at promoBanner component

--- src/components/ui/promo-banner.tsx
+++ src/components/ui/promo-banner.tsx
@@ -1,12 +1,37 @@
 import Image, { ImageProps } from 'next/image'
+import * as React from 'react'
+import { cva, type VariantProps } from 'class-variance-authority'
 
-const PromoBanner = ({ alt, ...props }: ImageProps) => {
+import { cn } from '@/lib/utils'
+
+const promoBannerVariants = cva('h-auto w-full rounded-lg', {
+  variants: {
+    variant: {
+      rounded: 'h-auto w-full rounded-lg',
+      roundedNone: 'h-auto w-full rounded-none',
+    },
+  },
+  defaultVariants: {
+    variant: 'rounded',
+  },
+})
+
+export interface PromoBannerProps
+  extends ImageProps,
+    VariantProps<typeof promoBannerVariants> {}
+
+const PromoBanner = ({
+  alt,
+  variant,
+  className,
+  ...props
+}: PromoBannerProps) => {
   return (
     <Image
       {...props}
       height={0}
       width={0}
-      className='h-auto w-full rounded-lg'
+      className={cn(promoBannerVariants({ variant }), className)}
       sizes='100vw'
       priority
       alt={alt}
